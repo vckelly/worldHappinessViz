309c94533e71c65e3afe673bfad0c82b94312d24
vckelly <vckelly@gmail.com>
2020-09-02 15:43:31 -0600

    feat: added individual parse funcs for each year of data

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function parseYear(filename) {
+function parse2015(filename) {
   return d3.dsv(",", filename, function(d) {
     return {
       country: d["Country"],
@@ -16,40 +16,81 @@ function parseYear(filename) {
     };
   })
 };
-let y2015 = parseYear("2015.csv");
-let y2016 = parseYear("2016.csv");
-let y2017 = parseYear("2017.csv");
-let y2018 = parseYear("2018.csv");
-let y2019 = parseYear("2019.csv");
-y2015.then((d) => console.log(d));
-y2017.then((d) => console.log(d));
 
-// const yearData = d3.dsv(",", "2015.csv", function(d) {
-//   return {
-//     country: d["Country"],
-//     region: d["Region"],
-//     hRank: d["Happiness Rank"],
-//     hScore: d["Happiness Score"],
-//     stdErr: d["Standard Error"],
-//     econ: d["Economy (GDP per Capita)"],
-//     family: d["Family"],
-//     health: d["Health (Life Expectancy)"],
-//     freedom: d["Freedom"],
-//     trust: d["Trust (Government Corruption)"],
-//     generosity: d["Generosity"],
-//     dystopia: d["Dystopia Residual"]
-//   };
-// }).then((data) => {
-//   console.log(data);
-// });
+function parse2016(filename) {
+  return d3.dsv(",", filename, function(d) {
+    return {
+      country: d["Country"],
+      region: d["Region"],
+      hRank: d["Happiness Rank"],
+      hScore: d["Happiness Score"],
+      econ: d["Economy (GDP per Capita)"],
+      family: d["Family"],
+      health: d["Health (Life Expectancy)"],
+      freedom: d["Freedom"],
+      trust: d["Trust (Government Corruption)"],
+      generosity: d["Generosity"],
+      dystopia: d["Dystopia Residual"]
+    };
+  })
+};
 
+function parse2017(filename) {
+  return d3.dsv(",", filename, function(d) {
+    return {
+      country: d["Country"],
+      hRank: d["Happiness.Rank"],
+      hScore: d["Happiness.Score"],
+      econ: d["Economy..GDP.per.Capita."],
+      family: d["Family"],
+      health: d["Health..Life.Expectancy."],
+      freedom: d["Freedom"],
+      trust: d["Trust..Government.Corruption."],
+      generosity: d["Generosity"],
+      dystopia: d["Dystopia.Residual"]
+    };
+  })
+};
 
-// const yearData = d3.dsv(",", "2015.csv")
-//                    .then(data => {
-//                      console.log(d3.csvParse(data))
-//                    });
-// console.log(yearData);
+function parse2018(filename) {
+  return d3.dsv(",", filename, function(d) {
+    return {
+      country: d["Country or region"],
+      hRank: d["Overall rank"],
+      hScore: d["Score"],
+      econ: d["GDP per capita"],
+      family: d["Social support"],
+      health: d["Healthy life expectancy"],
+      freedom: d["Freedom to make life choices"],
+      trust: d["Perceptions of corruption"],
+      generosity: d["Generosity"]
+    };
+  })
+};
 
+function parse2019(filename) {
+  return d3.dsv(",", filename, function(d) {
+    return {
+      country: d["Country or region"],
+      hRank: d["Overall rank"],
+      hScore: d["Score"],
+      econ: d["GDP per capita"],
+      family: d["Social support"],
+      health: d["Healthy life expectancy"],
+      freedom: d["Freedom to make life choices"],
+      trust: d["Perceptions of corruption"],
+      generosity: d["Generosity"]
+    };
+  })
+};
+let y2015 = parse2015("/data/2015.csv");
+let y2016 = parse2016("/data/2016.csv");
+let y2017 = parse2017("/data/2017.csv");
+let y2018 = parse2018("/data/2018.csv");
+let y2019 = parse2019("/data/2019.csv");
+y2015.then((d) => console.log("2015", d));
+y2016.then((d) => console.log("2016", d));
+y2017.then((d) => console.log("2017", d));
+y2018.then((d) => console.log("2018", d));
+y2019.then((d) => console.log("2019", d));
 
-//console.log(yearData);
-//console.log(d3.csvParse(yearData));
